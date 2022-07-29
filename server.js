8cad7b173a21118be61724ec55191afca35be7e8
zulaikhahanan <aikhx00@gmail.com>
2022-07-29 16:22:46 +0800

    make it better

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(session({
 // Using ejs engine template
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
-
+app.engine('ejs', require('ejs').renderFile);
 
 //Access to CSS,HTML from the Folder named 'public'
 app.use(express.static(path.join(__dirname, 'public')));
