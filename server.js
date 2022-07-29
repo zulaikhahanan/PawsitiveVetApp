3b435abf7993098cb2f545a1c1490894b6fd0503
zulaikhahanan <aikhx00@gmail.com>
2022-07-29 14:57:55 +0800

    hi

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(session({
 
 
 // Using ejs engine template
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
